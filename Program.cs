a24caa01438c5ae4e033d72a9ddb051a5add17da
Zero <shinyzero0@gmail.com>
2023-02-20 19:13:00 +0300

    init, reading file and writing stdout

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Joiner;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string[] data = File.ReadAllLines(args[0]);
+        int i = 0;
+        var paras = new List<string>();
+        while (i < data.Length)
+        {
+            string startLine = data[i];
+            if (!string.IsNullOrWhiteSpace(startLine))
+            {
+                int localI = i;
+                var paraLines = new List<string>();
+                string localLine = data[localI];
+                while (!string.IsNullOrWhiteSpace(localLine) && localI < data.Length)
+                {
+                    localLine = data[localI];
+                    paraLines.Add(localLine);
+                    localI++;
+                }
+                i = localI;
+                paras.Add(String.Join(" ", paraLines));
+            }
+            else
+            {
+                i++;
+            }
+
+        }
+        paras.ForEach(p => Console.WriteLine(p));
+        /* File.WriteAllLines("result.txt", paras); */
+    }
+    
+}
