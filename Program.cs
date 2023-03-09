d72711ea028e969c01474fbb8ea8e03129f85d72
zero mobile <shinyzero0@gmail.com>
2023-03-09 14:16:19 +0300

    init

--- Program.cs
+++ Program.cs
@@ -1,82 +1,85 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
-namespace sharpjoin;
-
-public class Program
+namespace sharpjoin
 {
+  public class Program
+  {
     public static void Main(string[] args)
     {
-        if (args.Length == 0)
-        {
-            Console.WriteLine("Usage: sharpjoin --join <file> or sharpjoin --split <file>");
-            return;
-        }
-        switch (args[0])
-        {
-            case "--join":
-                Join(args[1]);
-                break;
-            case "--split":
-                Split(args[1]);
-                break;
+      if (args.Length == 0)
+      {
+        Console.WriteLine("Usage: sharpjoin --join <file> or sharpjoin --split <file>");
+        return;
+      }
+      switch (args[0])
+      {
+        case "--join":
+          Join(args[1]);
+          break;
+        case "--split":
+          Split(args[1]);
+          break;
 
-            default:
-                Console.WriteLine("Usage: sharpjoin --join <file> or sharpjoin --split <file>");
-                break;
-        }
+        default:
+          Console.WriteLine("Usage: sharpjoin --join <file> or sharpjoin --split <file>");
+          break;
+      }
     }
 
     public static void Join(string source)
     {
-        string[] data = File.ReadAllLines(source);
-        int i = 0;
-        var paras = new List<string>();
-        while (i < data.Length)
+      string[] data = File.ReadAllLines(source);
+      int i = 0;
+      var paras = new List<string>();
+      while (i < data.Length)
+      {
+        string startLine = data[i];
+        if (!string.IsNullOrWhiteSpace(startLine))
         {
-            string startLine = data[i];
-            if (!string.IsNullOrWhiteSpace(startLine))
-            {
-                int localI = i;
-                var paraLines = new List<string>();
-                string localLine = data[localI];
-                while (!string.IsNullOrWhiteSpace(localLine) && localI < data.Length)
-                {
-                    localLine = data[localI];
-                    paraLines.Add(localLine);
-                    localI++;
-                }
-                i = localI;
-                paras.Add(Regex.Replace((String.Join(" ", paraLines)), " +$", ""));
-            }
-            else
-            {
-                i++;
-            }
+          int localI = i;
+          var paraLines = new List<string>();
+          string localLine = data[localI];
+          while (!string.IsNullOrWhiteSpace(localLine) && localI < data.Length)
+          {
+            localLine = data[localI];
+            paraLines.Add(localLine);
+            localI++;
+          }
+          i = localI;
+          paras.Add(Regex.Replace((String.Join(" ", paraLines)), " +$", ""));
         }
-        paras.ForEach(p => Console.WriteLine(p));
+        else
+        {
+          i++;
+        }
+      }
+      paras.ForEach(p => Console.WriteLine(p));
     }
 
     public static void Split(string source)
     {
-        string[] data = File.ReadAllLines(source);
-        var output = new List<string>();
-        foreach (string line in data)
-        {
-            if (string.IsNullOrWhiteSpace(line)) continue;
-            string sentence = Regex.Replace(
-                line,
-                @"([\.\?!]) [A-ZА-Я]",
-                match =>
-                {
-                    string m = match.Value;
-                    return string.Format("{0}\n{1}", m[0], m[2]);
-                }
+      string[] data = File.ReadAllLines(source);
+      var output = new List<string>();
+      foreach (string line in data)
+      {
+        if (string.IsNullOrWhiteSpace(line)) continue;
+        string sentence = Regex.Replace(
+            line,
+            @"([\.\?!]) [A-ZА-Я]",
+            match =>
+            {
+            string m = match.Value;
+            return string.Format("{0}\n{1}", m[0], m[2]);
+            }
             );
-            string result = string.Format("{0}\n", sentence);
-            output.Add(result);
-        }
-        output.ForEach(p => Console.WriteLine(p));
+        string result = string.Format("{0}\n", sentence);
+        output.Add(result);
+      }
+      output.ForEach(p => Console.WriteLine(p));
     }
+  }
 }
+// vim:ts=2:sw=2
