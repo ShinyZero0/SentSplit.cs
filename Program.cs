54df81c509401d0830bce9c35234ec2a05f9b983
zero mobile <shinyzero0@gmail.com>
2023-03-13 12:21:02 +0300

    Mono compatibility

--- Program.cs
+++ Program.cs
@@ -1,82 +1,84 @@
 ﻿using System;
+using System.IO; // Mono compatibility
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
-namespace sharpjoin;
-
-public class Program
-{
-    public static void Main(string[] args)
+namespace sharpjoin
+{ // Mono compatibility
+    public class Program
     {
-        if (args.Length == 0)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Usage: sharpjoin --join <file> or sharpjoin --split <file>");
-            return;
-        }
-        switch (args[0])
-        {
-            case "--join":
-                Join(args[1]);
-                break;
-            case "--split":
-                Split(args[1]);
-                break;
-
-            default:
+            if (args.Length == 0)
+            {
                 Console.WriteLine("Usage: sharpjoin --join <file> or sharpjoin --split <file>");
-                break;
-        }
-    }
-
-    public static void Join(string source)
-    {
-        string[] data = File.ReadAllLines(source);
-        int i = 0;
-        var paras = new List<string>();
-        while (i < data.Length)
-        {
-            string startLine = data[i];
-            if (!string.IsNullOrWhiteSpace(startLine))
-            {
-                int localI = i;
-                var paraLines = new List<string>();
-                string localLine = data[localI];
-                while (!string.IsNullOrWhiteSpace(localLine) && localI < data.Length)
-                {
-                    localLine = data[localI];
-                    paraLines.Add(localLine);
-                    localI++;
-                }
-                i = localI;
-                paras.Add(Regex.Replace((String.Join(" ", paraLines)), " +$", ""));
+                return;
             }
-            else
+            switch (args[0])
             {
-                i++;
+                case "--join":
+                    Join(args[1]);
+                    break;
+                case "--split":
+                    Split(args[1]);
+                    break;
+
+                default:
+                    Console.WriteLine("Usage: sharpjoin --join <file> or sharpjoin --split <file>");
+                    break;
             }
         }
-        paras.ForEach(p => Console.WriteLine(p));
-    }
 
-    public static void Split(string source)
-    {
-        string[] data = File.ReadAllLines(source);
-        var output = new List<string>();
-        foreach (string line in data)
+        public static void Join(string source)
         {
-            if (string.IsNullOrWhiteSpace(line)) continue;
-            string sentence = Regex.Replace(
-                line,
-                @"([\.\?!])\s+((--\s+)|(—\s+)?[A-ZА-Я])",
-                match =>
+            string[] data = File.ReadAllLines(source);
+            int i = 0;
+            var paras = new List<string>();
+            while (i < data.Length)
+            {
+                string startLine = data[i];
+                if (!string.IsNullOrWhiteSpace(startLine))
                 {
-                    var m = match.Groups;
-                    return string.Format("{0}\n{1}", m[1].ToString(), m[2].ToString());
+                    int localI = i;
+                    var paraLines = new List<string>();
+                    string localLine = data[localI];
+                    while (!string.IsNullOrWhiteSpace(localLine) && localI < data.Length)
+                    {
+                        localLine = data[localI];
+                        paraLines.Add(localLine);
+                        localI++;
+                    }
+                    i = localI;
+                    paras.Add(Regex.Replace((String.Join(" ", paraLines)), " +$", ""));
                 }
-            );
-            string result = string.Format("{0}\n", sentence);
-            output.Add(result);
+                else
+                {
+                    i++;
+                }
+            }
+            paras.ForEach(p => Console.WriteLine(p));
         }
-        output.ForEach(p => Console.WriteLine(p));
+
+        public static void Split(string source)
+        {
+            string[] data = File.ReadAllLines(source);
+            var output = new List<string>();
+            foreach (string line in data)
+            {
+                if (string.IsNullOrWhiteSpace(line)) continue;
+                string sentence = Regex.Replace(
+                        line,
+                        @"([\.\?!])\s+((--\s+)|(—\s+)?[A-ZА-Я])",
+                        match =>
+                        {
+                        var m = match.Groups;
+                        return string.Format("{0}\n{1}", m[1].ToString(), m[2].ToString());
+                        }
+                        );
+                string result = string.Format("{0}\n", sentence);
+                output.Add(result);
+            }
+            output.ForEach(p => Console.WriteLine(p));
+        }
     }
 }
